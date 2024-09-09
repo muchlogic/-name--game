afc266663316f354bdd07448f708af8483ff4a08
oelbaytam <oelbaytam@gmail.com>
2024-09-09 12:37:37 -0400

    added join path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Join from "./join";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
   },
+  {
+    path: "/join",
+    element: <Join />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
