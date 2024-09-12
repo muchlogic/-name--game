efaf9873098199f47427a3cee4d87a7a9a6e1dc7
muchlogic <line04404@gmail.com>
2024-09-12 17:00:30 -0400

    Updated styles on home page

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -31,36 +31,34 @@ export default function Home() {
   return (
     <>
       <div
-        className={`min-h-screen bg-[url(./public/homeBG.jpg)] flex flex-col justify-center items-center`}
+        className={`min-h-screen bg-[url(./public/homeBG.jpg)] flex justify-center`}
       >
-        <h1 className="text-white text-9xl font-serif">Cawhoot!</h1>
-        <nav className="mb-4">
-          <button
-            onClick={() => handleHost()}
-            className="bg-gray-800 text-white text-lg py-3 px-6 rounded-md"
-          >
-            Host Game
-          </button>
-        </nav>
-
-        <div className="text-center bg-white px-6 py-6 rounded-md border-2">
-          <form>
-            <input
-              type="text"
-              id="item"
-              className="mb-4 p-2 rounded-md text-center border-2"
-              value={roomID}
-              onChange={(e) => setroomID(e.target.value)}
-              placeholder="Enter Room ID"
-            />
-            <br />
-            <button
-              onClick={() => handleJoin()}
-              className="bg-gray-800 text-white text-lg py-3 px-6 rounded-md"
-            >
-              Join Room
-            </button>
-          </form>
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-white text-8xl font-serif py-10">Cawhoot!</h1>
+          <div className="text-center bg-white py-4 px-6 rounded-md flex flex-col justify-center items-center shadow-md shadow-black">
+            <form className="flex flex-col">
+              <input
+                type="text"
+                id="item"
+                className="p-4 rounded-md text-center border-[black] border-[1px]"
+                value={roomID}
+                onChange={(e) => setroomID(e.target.value)}
+                placeholder="Enter Room ID"
+              />
+              <button
+                onClick={() => handleJoin()}
+                className="bg-gray-800 text-white text-lg mt-4 py-4 rounded-md hover:bg-slate-900"
+              >
+                Join Room
+              </button>
+              <button
+                onClick={() => handleHost()}
+                className="bg-gray-800 text-white text-lg rounded-md mt-4 py-4 hover:bg-slate-900"
+              >
+                Host Game
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
