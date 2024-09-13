9529113bf26adbb2e71736b334c5fe02eb6b36e7
oelbaytam <oelbaytam@gmail.com>
2024-09-13 13:18:52 -0400

    Fixed: Cards display in list
    - Fixed functionality and cards now display in a list below the form.
    - Changed visuals for how cards are displayed.

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,8 +1,10 @@
-export default function Card(card) {
+/* eslint-disable react/prop-types */
+
+export default function Card({ card }) {
   return (
-    <div className="text-center bg-white  py-5 px-40 rounded-md flex flex-col justify-center items-center shadow-md shadow-black">
-      <h1>Card {card.number}</h1>
-      <h2>{card.question}</h2>
+    <div className="bg-white py-5 px-4 rounded-md grid grid-cols-2 justify-center items-center shadow-md shadow-black">
+      <h1 className="col-span-2">Card {card.number}</h1>
+      <h2 className="col-span-2">{card.question}</h2>
       <p>{card.a1}</p>
       <p>{card.a2}</p>
       <p>{card.a3}</p>
--- client/src/routes/CreateCard.jsx
+++ client/src/routes/CreateCard.jsx
@@ -3,31 +3,20 @@ import Card from "../components/Card";
 
 export default function Create() {
   const [cards, setCards] = useState([]);
+  const [number, setNumber] = useState(1);
   const [question, setQuestion] = useState("");
   const [a1, setA1] = useState("");
   const [a2, setA2] = useState("");
   const [a3, setA3] = useState("");
   const [a4, setA4] = useState("");
-  // text-center bg-white  py-5 px-40 rounded-md flex flex-col justify-center items-center shadow-md shadow-black
-  const Card = ({ number, question, a1, a2, a3, a4 }) => {
-    <li className="">
-      <h1>Card {number}</h1>
-      <h2>{question}</h2>
-      <p>{a1}</p>
-      <p>{a2}</p>
-      <p>{a3}</p>
-      <p>{a4}</p>
-    </li>;
-  };
 
-  let cardNumber = 0;
   function SubmitCard(e) {
     e.preventDefault();
-    cardNumber += 1;
+    setNumber(number + 1);
     setCards((prev) => [
       ...prev,
       {
-        number: cardNumber,
+        number: number,
         question: question,
         a1: a1,
         a2: a2,
@@ -44,10 +33,12 @@ export default function Create() {
 
   return (
     <>
-      <div className={`bg-[url(./public/homeBG.jpg)] flex justify-center`}>
+      <div
+        className={`bg-[url(./public/homeBG.jpg)] flex flex-col justify-center`}
+      >
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-white text-8xl font-serif py-10">Create Card</h1>
-          <div className="text-center bg-white  py-5 px-40 rounded-md flex flex-col justify-center items-center shadow-md shadow-black">
+          <div className="text-center bg-white rounded-md flex flex-col justify-center items-center shadow-md shadow-black">
             <form className="flex flex-col">
               <input
                 type="text"
@@ -95,17 +86,17 @@ export default function Create() {
               </div>
               <button
                 onClick={(e) => SubmitCard(e)}
-                className="bg-gray-800 text-white text-lg rounded-md mt-4 py-4 hover:bg-slate-900"
+                className="bg-gray-800 text-white text-lg rounded-md m-4 py-4 hover:bg-slate-900"
               >
                 Submit Card
               </button>
             </form>
           </div>
-          <ul id="Created" className="flex flex-col">
-            {cards.map((card) => {
-              <Card card={card} />;
-            })}
-          </ul>
+        </div>
+        <div id="Created" className="grid grid-cols-2 gap-4 m-4">
+          {cards.map((card, index) => {
+            return <Card key={index} card={card} />;
+          })}
         </div>
       </div>
     </>
